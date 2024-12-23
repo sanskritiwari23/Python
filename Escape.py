e6ea7554e0eecea21acabb12364161dae764f759
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-23 14:22:34 +0530

    Add files via upload

---
+++ Escape.py
@@ -0,0 +1,17 @@
+# Hey Raji, Please don't remove this line
+# Author: Harry
+print("Hey I'm a good boy\nand this viewer is also a good girl/boy")
+
+#Escape Sequence Character
+#To insert character that cannot be directly used in a string, we use an escape sequence character.
+#An escape sequence is a backslash "\" followed by the character you want to insert
+print("Hey I'm a \"good boy\" and this viewer is also a good girl/boy") #\" good boy\" is used to put double courts in your sentence!!
+
+print("hey", 6, 7) #inside a single print statement I can peint multiple things
+print("hey", 6, 7, sep="~") #If you want to seperate the statements using any sing just use sep="" and put that sign inside double courts
+
+print("Hey", 6, 7, end="009") #end="" statement specifies what to print at the end, Default is '\n'
+print("Harry")
+
+print("Hey", 6, 7, end="009\n") #end can be used in both the ways 
+print("Harry")
