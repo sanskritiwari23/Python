3f1447f6b58b670abc1d7fa7740a34aa9f39a8a9
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-23 16:09:01 +0530

    Update TypeConversion.py

--- TypeConversion.py
+++ TypeConversion.py
@@ -4,3 +4,10 @@ b = 4.25
 sum = a + b #Type conversion of a(int) is done in this step
 print(sum)
 print(type(a))
+
+#The conversion of one datatype into the other datatype is known as type casting in python or type conversion in python
+
+#Methods for conversion = int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict()
+
+#Explicit Conversion - Done by the programmer
+#Implicit Conversion - Done by the python itself, this is done according to the level of ordering of datatypes in python
