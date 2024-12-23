8828b51d265213b72b7fe2c1387340ba9b09f30a
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-23 16:16:48 +0530

    Update Input.py

--- Input.py
+++ Input.py
@@ -5,3 +5,9 @@ print(type(name), name) #Input is always a string no matter what the datatype mu
 
 name2 = input("Enter your age: ")
 print("You Entered ", name2)
+
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+
+print(x + y)#Concination
+print(int(x) + int(y))#Addission
