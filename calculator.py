3cdb8fa4eda48e2a8f30dffef1e34c19912a5b3a
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-23 15:54:06 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+
+
+print (a + b)
+
+print(a - b)
+
+print(a * b)
+
+print(a / b)
+
+print(a % b)
+
+print(a // b)
+
+print(a ** b)
