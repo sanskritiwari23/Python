1e7e284c1c829e40708065428335dfd9efacaf8a
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-24 18:42:58 +0530

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -21,3 +21,12 @@ else:
 
 
 # https://docs.python.org/3/library/time.html#time.strftime
+
+#Another method using int!!!!
+timestamp = int(time.strftime('%H'))
+if(timestamp < 12):
+    print("Good Morning")
+elif(timestamp < 17):
+    print("Good Afternoon")
+else:
+    print("Good Evening")
