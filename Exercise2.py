ea644373203f0ba601361c1f41d536ab802414dc
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-24 18:37:15 +0530

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,23 @@
+#Good Morning Sir
+
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+# timestamp = time.strftime('%H')
+# # print(timestamp)
+# # timestamp = time.strftime('%M')
+# # print(timestamp)
+# # timestamp = time.strftime('%S')
+# # print(timestamp)
+
+#While comparing the time it is very imp to compare it in the form of HH:MM:SS
+#There must always be '' used while comparing the time
+if(timestamp < '12:00:00'):
+    print("Good Morning")
+elif(timestamp < '17:00:00'):
+    print("Good Afternoon")
+else:
+    print("Good Evening")
+
+
+# https://docs.python.org/3/library/time.html#time.strftime
