0fb822ecc97a0e1ce31c37e477065d32fa99b78e
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2024-12-26 18:23:44 +0530

    Add files via upload

---
+++ Loops2.py
@@ -0,0 +1,20 @@
+#WHILE LOOP
+# i = int(input("Enter the Number: "))
+i = 0
+while(i<=38):
+    i = int(input("Enter the Number: "))
+    print(i)
+
+print('Done with the loop')
+#While Loops execute statements while the condition is True. As soon as the condition becomes False, the interpreter comes out of the whileloop
+
+#Decerementing while loop
+count = 5
+while (count>=0):
+    print(count)
+    count = count - 1
+else:
+    print("Iam inside else")
+#Else can be used with while loop only, as soon as the while loop ends the iterepreter comes out of the while loop else gets print
+
+
