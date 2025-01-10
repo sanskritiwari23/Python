25845319a252af1fa0467c0c88f6d132eb286e9b
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2025-01-10 11:16:52 +0530

    Add files via upload

---
+++ shortifelse.py
@@ -0,0 +1,9 @@
+#Short Hand if-else
+a = 330
+b = 3303
+print ("A") if a>b else print("=") if a ==b else print("B")
+
+print(9) if a<b else"" #USing "" if we do not want to print anything after else, and you can not miss else otherwise it'll give an error
+
+c = 9 if a>b else 0
+print(c)
