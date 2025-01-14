df14e308c62729cd92e126cd018be40562cbdb9a
Sanskriti Tiwari <74483881+sanskritiwari23@users.noreply.github.com>
2025-01-14 11:27:24 +0530

    Add files via upload

---
+++ harry.py
@@ -0,0 +1,6 @@
+def welcome():
+    print("Hey you are welcome from harry")
+
+print(__name__)
+if __name__ == "__main__":#This will maintain that the O/P is printed once, irrespective of how it is written
+    welcome()
---
+++ main.py
@@ -0,0 +1,3 @@
+import harry
+
+harry.welcome() #Using this it'll print the same O/P twice, therefore we can remove this and by using only harry, it'll provide us the desired O/P
